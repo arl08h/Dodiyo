49e091b31919637ee0109f33defcffe98268ecc2
Andrew <andrew.lahaye@gmail.com>
2018-04-22 14:20:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import './stylesheets/site.css.scss';
+import './stylesheets/color.css';
+import './stylesheets/404.css';
+import './javascripts/typed';
---
+++ javascripts/typed.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var typed = new Typed('#typed', {
+  strings: ['Design', 'Optimize', 'Magic'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 800,
+  startDelay: 1500,
+  loop: false,
+});
